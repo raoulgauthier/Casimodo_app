b461f9371f871345db17b0342cf362bac5a2733d
Raoul Gauthier <raoul_gauthier@outlook.com>
2018-03-27 16:05:33 +0200

    landing page and home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
 
 
+
+
+  authenticated :user do
+    root 'home#index'
+  end
+
   resources :workorders
 
-
-
-
-
-
   devise_for :users
-  root to: 'home#index'
+  root to: 'landing_page#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
