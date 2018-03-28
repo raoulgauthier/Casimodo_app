d056d1ee9b67969bb856d78af50df8ba1a1094ce
Raoul Gauthier <raoul_gauthier@outlook.com>
2018-03-28 11:54:01 +0200

    problem fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users do
-  root to: 'landing_page#index'
-  end
+
 
   authenticated :user do
     root 'home#index'
@@ -21,6 +19,9 @@ Rails.application.routes.draw do
       post 'report'
     end
   end
+
+devise_for :users
+  root to: 'landing_page#index'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
