765707c7667f00d8e80bbee868e2c53184030440
Pascal Fievet <pascalfievet@gmail.com>
2018-03-29 14:37:03 +0200

    date filter done

--- app/controllers/workorders_controller.rb
+++ app/controllers/workorders_controller.rb
@@ -5,7 +5,7 @@ class WorkordersController < ApplicationController
 
   # GET /workorders
   def index
-
+    status_due_date
     if params[:technician].nil? && params[:manager].nil? & params[:status].nil? & params[:date_planned].nil?
       @workorders = Workorder.all
     else
@@ -13,7 +13,9 @@ class WorkordersController < ApplicationController
       @tech = params[:technician][:id].blank? ? "" : query[:technician] = User.find(params[:technician][:id])
       @manager =  params[:manager][:id].blank? ? "" : query[:manager] = User.find(params[:manager][:id])
       @status = params[:status].blank? ? "" : query[:status] = params[:status]
-      @date_planned = params[:date_planned].blank? ? "" : query[:date_planned] = DateTime.parse(params[:date_planned]["{}(1i)"] +"-"+ params[:date_planned]["{}(2i)"] +"-"+ params[:date_planned]["{}(3i)"]).all_day
+      if params[:due_date_selector] == @due_date_selector[1]
+        @date_planned = params[:date_planned].blank? ? "" : query[:date_planned] = DateTime.parse(params[:date_planned]["{}(1i)"] +"-"+ params[:date_planned]["{}(2i)"] +"-"+ params[:date_planned]["{}(3i)"]).all_day
+      end
       p query[:date_planned]
       @workorders = Workorder.where(query)
     end
@@ -23,6 +25,7 @@ class WorkordersController < ApplicationController
   end
 
   def filter
+     status_due_date
   end
 
   def new
@@ -57,6 +60,10 @@ class WorkordersController < ApplicationController
   end
 
   private
+
+    def status_due_date
+      @due_date_selector = ["Do not filter on due date", "Filter on due date"]
+    end
     # Use callbacks to share common setup or constraints between actions.
     def set_workorder
       @workorder = Workorder.find(params[:id])
