a6a702c0be20dedcdf9174e190148bfc3288b068
Raoul Gauthier <raoul_gauthier@outlook.com>
2018-03-29 19:07:44 +0200

    fixed erro 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ User.delete_all
 users = User.create([
 { email: 'Don.Draper@gmail.com', password: '123456', phone: '33 1 23 45 67 89', user_type: 'manager' },
 { email: 'Roger.Sterling@gmail.com', password: '123456', phone: '33 1 23 45 67 89', user_type: 'manager' },
-{ email: 'Peggy.Olson@gmail.com', password: '123456', phone: '33 1 23 45 67 89', user_type: 'manager' },
+{ email: 'rarajr@hotmail.com.com', password: '123456', phone: '33 1 23 45 67 89', user_type: 'manager' },
 { email: 'Pete.Campbell@gmail.com', password: '123456', phone: '33 1 23 45 67 89', user_type: 'manager' },
 { email: 'Joan.Harris@gmail.com', password: '123456', phone: '33 1 23 45 67 89', user_type: 'manager' },
 { email: 'Walter.White@gmail.com', password: '123456', phone: '33 1 23 45 67 89', user_type: 'technician' },
@@ -30,8 +30,9 @@ users = User.create([
 { email: 'Ted.Beneke@gmail.com', password: '123456', phone: '33 1 23 45 67 89', user_type: 'technician' },
 { email: 'Asac.Merkert@gmail.com', password: '123456', phone: '33 1 23 45 67 89', user_type: 'technician' },
 { email: 'Tyrus.Kitt@gmail.com', password: '123456', phone: '33 1 23 45 67 89', user_type: 'technician' },
-{ email: 'rarajr@hotmail.com', password: '123456', phone: '33 1 23 45 67 89', user_type: 'technician' }
+{ email: 'rarajr@hotmail.com', password: '123456', phone: '33 1 23 45 67 89', user_type: 'technician' },
 ])
+puts "helllo raoul"
 workorders = Workorder.create([
 
 { manager: users[0], technician: users[7], address: '5 place Sainte-Gudule, bruxelles', description: 'ascenceur de gauche bloqué au RDC', date_planned: '2018,3,27,10,15', status:'Scheduled' },
