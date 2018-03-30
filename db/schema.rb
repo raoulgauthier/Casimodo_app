6a752b2953e5d3e016462d9b1160a1b75db1fe83
Felix Verhulst <felix.verhulst@hotmail.fr>
2018-03-30 16:25:40 +0200

    usernames implemented

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180330101532) do
+ActiveRecord::Schema.define(version: 20180330134510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20180330101532) do
     t.string "invited_by_type"
     t.bigint "invited_by_id"
     t.integer "invitations_count", default: 0
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
     t.index ["invitations_count"], name: "index_users_on_invitations_count"
@@ -70,8 +71,8 @@ ActiveRecord::Schema.define(version: 20180330101532) do
     t.datetime "date_done"
     t.text "report"
     t.string "status"
-    t.integer "manager_id"
-    t.integer "technician_id"
+    t.bigint "manager_id"
+    t.bigint "technician_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "date_started"
@@ -80,4 +81,6 @@ ActiveRecord::Schema.define(version: 20180330101532) do
   end
 
   add_foreign_key "photos", "workorders"
+  add_foreign_key "workorders", "users", column: "manager_id"
+  add_foreign_key "workorders", "users", column: "technician_id"
 end
